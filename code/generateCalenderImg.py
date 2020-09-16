9114b00c06fba04c0e533c23aa059ad27eff4e5e
醉家男煮饺 <88431844@163.com>
2020-09-16 14:00:43 +0800

    python生成图片暂时完成

--- code/generateCalenderImg.py
+++ code/generateCalenderImg.py
@@ -8,11 +8,12 @@ def drawMonth(month=1):
              'July', 'August', 'September', 'October', 'November', 'December')
 
     # create new blank picture
-    img = Image.new('RGB', size=(1920, 1080), color=(255,255,255))
+    img = Image.new('RGB', size=(1920, 1080), color=(255, 255, 255))
     width, height = img.size
     # rows = 2 titles + 5 rows of days + 2(head + footer)blank
     # cols = 7 cols of week + 1 blank for left + 3 col for pic
-    rows, cols = 9, len(WEEK) + 4
+    # rows, cols = 9, len(WEEK) + 4
+    rows, cols = 7, len(WEEK) + 2
     colSpace, rowSpace = width // cols, height // rows
 
     # paste your img on the right, 549*1080
@@ -24,19 +25,21 @@ def drawMonth(month=1):
     month_font = r'C:\Users\GW00191732\PycharmProjects\Ecalender\font\font-f930.ttc'
     title_font = r'C:\Users\GW00191732\PycharmProjects\Ecalender\font\font-f930.ttc'
     day_font = r'C:\Users\GW00191732\PycharmProjects\Ecalender\font\font-f930.ttc'
-    month_size, title_size, day_size = 80, 58, 34
+    month_size, title_size, day_size = 80, 60, 60
 
     draw = ImageDraw.Draw(img)
     for i in range(len(WEEK) + 1):
         # draw month title
         if i == 0:
-            draw.text((colSpace, rowSpace), MONTH[month-1], fill=(0,0,0,), font=ImageFont.truetype(month_font, size=month_size))
+            draw.text((colSpace, rowSpace), MONTH[month - 1], fill=(0, 0, 0,),
+                      font=ImageFont.truetype(month_font, size=month_size))
             top = rowSpace // 10
-            draw.line(xy=[(colSpace, rowSpace*2-top * 2), (colSpace*7.5, rowSpace*2-top * 2)], fill=(0,0,0))
+            draw.line(xy=[(colSpace, rowSpace * 2 - top * 2), (colSpace * 7.5, rowSpace * 2 - top * 2)], fill=(0, 0, 0))
             draw.line(xy=[(colSpace, rowSpace * 2 - top * 1), (colSpace * 7.5, rowSpace * 2 - top * 1)], fill=(0, 0, 0))
             continue
         # draw week title
-        draw.text((colSpace*i, rowSpace*2), WEEK[i-1], fill=(0,0,0), font=ImageFont.truetype(title_font, size=title_size))
+        draw.text((colSpace * i, rowSpace * 2), WEEK[i - 1], fill=(0, 0, 0),
+                  font=ImageFont.truetype(title_font, size=title_size))
 
     # draw days
     cal = calendar.Calendar(firstweekday=0)
@@ -48,16 +51,18 @@ def drawMonth(month=1):
                 fill = (255, 0, 0)
             else:
                 fill = (0, 0, 0)
-            draw.text((colSpace * col + day_size, rowSpace * row), str(day), fill=fill, font=ImageFont.truetype(day_font, size=day_size))
+            draw.text((colSpace * col + day_size, rowSpace * row), str(day), fill=fill,
+                      font=ImageFont.truetype(day_font, size=day_size))
         col += 1
         # to a new week
         if col == 8:
             col = 1
             row += 1
 
-    img = img.resize((400,300), Image.ANTIALIAS)
-    img.save(MONTH[month-1] + '.png')
+    img = img.resize((400, 300), Image.ANTIALIAS)
+    # img.save(MONTH[month-1] + '.png')
+    img.show()
+
 
 if __name__ == '__main__':
     drawMonth(month=4)
-
