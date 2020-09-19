0cd48c700cfda69406435352a418f28624adca83
醉家男煮饺 <88431844@163.com>
2020-09-19 15:56:18 +0800

    周末红色显示

--- code/eCalender.py
+++ code/eCalender.py
@@ -14,7 +14,7 @@ sys.setdefaultencoding('utf8')
 fontPath = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'font')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
-    sys.path.append(libdir)
+	sys.path.append(libdir)
 
 import logging
 from epd_driver import epd4in2_GD
@@ -23,15 +23,16 @@ from PIL import Image, ImageDraw, ImageFont
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def getWeather():
-    r = requests.get('http://restapi.amap.com/v3/weather/weatherInfo?city=130606&key=446bdde0691cffb14c1aff83a8912467')
-    r.encoding = 'utf-8'
-    weatherData = json.loads(r.text.encode('utf-8'))
-    w = weatherData['lives'][0]
-    temperature = str(w['temperature'].encode('utf-8'))
-    weather = str(w['weather'].encode('utf-8'))
-    reportTime = str(w['reporttime'].encode('utf-8'))
-    return temperature, weather, reportTime
+	r = requests.get('http://restapi.amap.com/v3/weather/weatherInfo?city=130606&key=446bdde0691cffb14c1aff83a8912467')
+	r.encoding = 'utf-8'
+	weatherData = json.loads(r.text.encode('utf-8'))
+	w = weatherData['lives'][0]
+	temperature = str(w['temperature'].encode('utf-8'))
+	weather = str(w['weather'].encode('utf-8'))
+	reportTime = str(w['reporttime'].encode('utf-8'))
+	return temperature, weather, reportTime
 
 
 try:
@@ -48,52 +49,46 @@ try:
 	nowDay = int(time.strftime('%d'))
 
 	WEEK = ('星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日')
-	MONTH = ('一月', '二月', '三月', '四月', '五月', '六月',
-			 '七月', '八月', '九月', '十月', '十一月', '十二月')
+	MONTH = ('一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月')
 
 	# create new blank picture
-	img = Image.new('1', (epd.width, epd.height), 255)
-	HRYimage = Image.new('1', (epd.width, epd.height), 255)  # 298*126  ryimage: red or yellow image
-	drawry = ImageDraw.Draw(HRYimage)
-	drawry.rectangle((80, 50, 130, 100), fill=0)
-	width, height = img.size
+	blackImg = Image.new('1', (epd.width, epd.height), 255)
+	blackDraw = ImageDraw.Draw(blackImg)
+	redImg = Image.new('1', (epd.width, epd.height), 0)
+	redDraw = ImageDraw.Draw(redImg)
+
+	width, height = blackImg.size
 	# rows = 2 titles + 5 rows of days + 2(head + footer)blank
 	# cols = 7 cols of week + 1 blank for left + 3 col for pic
 	# rows, cols = 9, len(WEEK) + 4
 	rows, cols = 8, len(WEEK) + 2
 	colSpace, rowSpace = width // cols, height // rows
 
-	# paste your img on the right, 549*1080
-	# bgImg = Image.open('cat1.jpg')
-	# bgWidth, _ = bgImg.size
-	# img.paste(bgImg, box=(width-bgWidth, 0))
-
 	# define font and size
 	month_font = os.path.join(fontPath, 'font-old.ttc')
 	weather_font = os.path.join(fontPath, 'font-old.ttc')
 	week_font = os.path.join(fontPath, 'font-old.ttc')
-	day_work_font = os.path.join(fontPath, 'font-f930.ttc')
-	day_rest_font = os.path.join(fontPath, 'font-old.ttc')
+	day_font = os.path.join(fontPath, 'font-f930.ttc')
 	month_size, week_size, day_size, weather_size = 16, 13, 16, 13
-
-	draw = ImageDraw.Draw(img)
 	for i in range(len(WEEK) + 1):
 		# draw month title
 		if i == 0:
 			temperature, weather, reportTime = getWeather()
-			draw.text((colSpace, rowSpace + 20), u'温度:' + temperature + u'|天气:' + weather + u'|最后更新:' + reportTime,
-                      fill=0,
-                      font=ImageFont.truetype(weather_font, size=weather_size))
-			draw.text((colSpace, rowSpace ), u' ' + MONTH[month - 1],
-					  fill=0,
-					  font=ImageFont.truetype(month_font, size=month_size))
+			blackDraw.text((colSpace, rowSpace + 20), u'温度:' + temperature + u'|天气:' + weather + u'|最后更新:' + reportTime,
+			               fill=0,
+			               font=ImageFont.truetype(weather_font, size=weather_size))
+			blackDraw.text((colSpace, rowSpace), u' ' + MONTH[month - 1],
+			               fill=0,
+			               font=ImageFont.truetype(month_font, size=month_size))
 			top = rowSpace // 10
-			draw.line(xy=[(colSpace, rowSpace * 2 - top * 2 + 5), (colSpace * 8, rowSpace * 2 - top * 2 + 5)], fill=0)
-			draw.line(xy=[(colSpace, rowSpace * 2 - top * 1 + 5), (colSpace * 8, rowSpace * 2 - top * 1 + 5)], fill=0)
+			blackDraw.line(xy=[(colSpace, rowSpace * 2 - top * 2 + 5), (colSpace * 8, rowSpace * 2 - top * 2 + 5)],
+			               fill=0)
+			blackDraw.line(xy=[(colSpace, rowSpace * 2 - top * 1 + 5), (colSpace * 8, rowSpace * 2 - top * 1 + 5)],
+			               fill=0)
 			continue
 		# draw week title
-		draw.text((colSpace * i , rowSpace * 2 + 10 ), u' ' + WEEK[i - 1], fill=0,
-				  font=ImageFont.truetype(week_font, size=week_size))
+		blackDraw.text((colSpace * i, rowSpace * 2 + 10), u' ' + WEEK[i - 1], fill=0,
+		               font=ImageFont.truetype(week_font, size=week_size))
 
 	# draw days
 	cal = calendar.Calendar(firstweekday=0)
@@ -103,18 +98,19 @@ try:
 		if day > 0:
 			# if weekday, draw with red color
 			if col == 6 or col == 7:
-				fill = 0
-				day_font = day_rest_font
+				fill = 255
+				redDraw.text((colSpace * col + day_size, rowSpace * row), str(day), fill=fill,
+				             font=ImageFont.truetype(day_font, size=day_size))
 			else:
 				fill = 0
-				day_font = day_work_font
-			draw.text((colSpace * col + day_size, rowSpace * row), str(day), fill=fill,
-					  font=ImageFont.truetype(day_font, size=day_size))
+				blackDraw.text((colSpace * col + day_size, rowSpace * row), str(day), fill=fill,
+				               font=ImageFont.truetype(day_font, size=day_size))
+
 		# 判断输出日期是否为当天，是则在下面话点标识
 		if nowDay == str(day):
 			shape = [(colSpace * col + day_size + 5, rowSpace * row + 25),
-					 (colSpace * col + day_size + 10, rowSpace * row + 20)]
-			draw.rectangle(shape, fill=0)
+			         (colSpace * col + day_size + 10, rowSpace * row + 20)]
+			blackDraw.rectangle(shape, fill=0)
 
 		col += 1
 		# to a new week
@@ -122,25 +118,19 @@ try:
 			col = 1
 			row += 1
 
-		# print("x:" + str(colSpace * col + day_size), "y:" + str(rowSpace * row))
-		# print("nowDay:" + nowDay ,"day:" + str(day))
+	flipBlackImg = blackImg.transpose(Image.FLIP_LEFT_RIGHT)
+	rotateBlackImg = flipBlackImg.rotate(180)
 
-	# shape = [(1551 + 20, 405 + 80), (1551 + 40 , 405 + 60)]
-	# draw.rectangle(shape, fill=0)
+	flipRedImg = redImg.transpose(Image.FLIP_LEFT_RIGHT)
+	rotateRedImg = flipRedImg.rotate(180)
 
-	# img = img.resize((400, 300), Image.ANTIALIAS)
-	# img.save(MONTH[month-1] + '.png')
-
-	flipImg = img.transpose(Image.FLIP_LEFT_RIGHT)
-	rotateImg = flipImg.rotate(180)
-	# epd.displayBlack(epd.getbuffer(rotateImg))
-	epd.display(epd.getbuffer(rotateImg),epd.getbuffer(HRYimage))
+	epd.display(epd.getbuffer(rotateBlackImg), epd.getbuffer(rotateRedImg))
 
 
 except IOError as e:
-    logging.info(e)
+	logging.info(e)
 
 except KeyboardInterrupt:
-    logging.info("ctrl + c:")
-    epd4in2_GD.epdconfig.module_exit()
-    exit()
+	logging.info("ctrl + c:")
+	epd4in2_GD.epdconfig.module_exit()
+	exit()
